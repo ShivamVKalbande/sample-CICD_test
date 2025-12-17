62478141ac4f6898b5d379aca520ac2a619385b0
shivam <shivamkalbande@gmail.com>
2025-12-17 11:18:26 +0530

    testing accordance

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
     <section id="about" className="py-20 bg-gray-100 text-center">
       <h2 className="text-3xl font-bold">About Us</h2>
       <p className="mt-4 max-w-3xl mx-auto text-gray-600">
-        We build cutting-edge rockets and space technologies through innovation and engineering.
+        We build cutting-edge rockets and space technologies through innovation and engineering. Experienced in  it.
       </p>
     </section>
   );
